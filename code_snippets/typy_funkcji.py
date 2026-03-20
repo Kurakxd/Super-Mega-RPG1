2b41e7dd0680093b540a109f2bfd6afa795828dd
marek <marek.jastrzebski@outlook.com>
2026-03-20 18:18:04 +0100

    przykłady funkcji void i return

--- code_snippets/typy_funkcji.py
+++ code_snippets/typy_funkcji.py
@@ -18,6 +18,31 @@ print(wynik_mnozenia)
 def dodawanieVoid(licza1, liczba2):
     wynik = licza1+liczba2
 
+#Funkcja może zwrócic też dwa argumenty
+def info_o_userz(imie, nazwisko, rok_urodzenia):
+    wiek = 2026 - rok_urodzenia;
+    pierwsza_litera_imie = imie[0]
+    pierwsza_litera_nazwisko = nazwisko[0]
+    return pierwsza_litera_imie, pierwsza_litera_nazwisko, wiek
+
+dane = info_o_userz("Marek", "Jastrz", 1985)
+print("dane")
+print(dane)
+
+print("Dane ale z indeksami")
+print(dane[0])
+print(dane[1])
+print(dane[2])
+
+
+print("Print a,b,c pojedynczo")
+imie,naz,rok = info_o_userz("Marek", "Jastrz", 1985)
+print("imie:" + imie)
+print("naz:" + naz)
+print("rok" + str(rok))
+
+
+
 # tutaj będzie błąd bo nie można dodać liczy i Nono
 wynik_void = dodawanieVoid(4, 5)
 dodawanie_voida = dodwanieReturn(wynik_mnozenia, wynik_void)
